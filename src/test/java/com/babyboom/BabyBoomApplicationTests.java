9a14723a69de80c198962ea6056b341387039607
RenzzoGutierrez20 <u202014777@upc.edu.pe>
2023-05-26 20:58:03 -0600

    create aplication BabyBoom backend
    create project

---
+++ src/test/java/com/babyboom/BabyBoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyboom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyBoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
