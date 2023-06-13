8d8ed787b74340d2cd34f2ec1c38e8c49a3943c0
Stephan-5 <angiesr5@hotmail.com>
2023-06-13 00:31:42 -0500

    Problema terminado

---
+++ src/main/java/com/babyboom/servicesimplement/ProblemaServiceImplement.java
@@ -0,0 +1,29 @@
+package com.babyboom.servicesimplement;
+import com.babyboom.entities.Problema;
+import com.babyboom.repositories.IProblemaRepository;
+import com.babyboom.services.IProblemaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class ProblemaServiceImplement implements IProblemaService {
+
+    @Autowired
+    private IProblemaRepository pR;
+
+    @Override
+    public void insert(Problema problema) {pR.save(problema);}
+
+    @Override
+    public List<Problema> list() {
+        return pR.findAll();
+    }
+
+    @Override
+    public List<Problema> buscarFecha(LocalDate fecha) {
+        return pR.findByFechaInicio(fecha);
+    }
+}
